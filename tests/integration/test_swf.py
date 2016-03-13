f6287a8853a1968ab0ed53d5106298df11a407b0
Anne Matthies <amatthies@babbel.com>
2016-03-13 02:20:52 +0100

    docprint decorator to make integration test easier to follow

--- tests/integration/test_swf.py
+++ tests/integration/test_swf.py
@@ -1,3 +1,5 @@
+import json
+
 from deciders.test_01 import test_01
 from deciders.test_02 import test_02
 from deciders.test_03 import test_03
@@ -14,9 +16,9 @@ from deciders.test_13 import test_13
 from deciders.test_14 import test_14
 from deciders.test_15 import test_15
 from deciders.test_16 import test_16
+from test_helper import get_activity_result, docprint
 
 from activity_worker import ActivityWorkerProcess
-from test_helper import get_activity_result
 
 worker1 = ActivityWorkerProcess(domain='floto_test', task_list='floto_activities')
 worker2 = ActivityWorkerProcess(domain='floto_test', task_list='floto_activities')
@@ -24,125 +26,213 @@ worker1.start()
 worker2.start()
 
 
+@docprint
 def run_01():
-    # Single task with context
+    """
+    Test 01
+    Single task with context
+
+    """
     result = test_01()
     result = get_activity_result(result, 'activity1', 'v5')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['workflow'] == {'foo': 'bar'}
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_02():
-    # Single task without context
+    """Test 01
+    Single task without context
+
+    """
     result = test_02()
     result = get_activity_result(result, 'activity2', 'v4')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_03():
-    # Two tasks without dependency, run in parallel if > 1 worker
+    """Test 03
+    Two tasks without dependency, run in parallel if > 1 worker
+
+    """
     result = test_03()
     result1 = get_activity_result(result, 'activity1', 'v5')
     result2 = get_activity_result(result, 'activity2', 'v4')
+    print('Result 1: ' + json.dumps(result1) + '\n')
+    print('Result 2: ' + json.dumps(result2) + '\n')
 
     assert result1['workflow'] == {'foo': 'bar'}
     assert result1['status'] == 'finished'
     assert result2['status'] == 'finished'
 
 
+@docprint
 def run_04():
-    # Two tasks with 1 -> 3
+    """Test 04
+    Two tasks with 1 -> 3
+
+    """
     result = test_04()
     result3 = get_activity_result(result, 'activity3', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
 
     assert result3['activity1']['status'] == 'finished'
     assert result3['activity1']['workflow'] == {'foo': 'bar'}
     assert result3['status'] == 'finished'
 
 
+@docprint
 def run_05():
-    # Failing task with retry strategy, succeeds after retry
+    """Test 05
+    Failing task with retry strategy, succeeds after retry
+
+    """
     result = test_05()
     result = get_activity_result(result, 'activity_fails_3', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['workflow_input'] == {'foo': 'bar'}
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_06():
-    # Failing task with retry strategy, reaches limit of retries
+    """Test 06
+    Failing task with retry strategy, reaches limit of retries
+
+    """
     details = test_06()
     details = get_activity_result(details, 'activity_fails_2', 'v2')
+    print('Result: ' + json.dumps(details) + '\n')
     assert details == 'Something went wrong'
 
 
+@docprint
 def run_07():
-    # Timeout
+    """Test 07
+    Timeout
+
+    """
     result = test_07()
     result = get_activity_result(result, 'activity2', 'v4')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_08():
-    # Repeated Workflow
+    """Test 08
+    Repeated Workflow
+
+    """
     result = test_08()
     result = get_activity_result(result, 'activity1', 'v5')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_09():
-    # Repeated Workflow with timer and failing activity with retries
+    """Test 09
+    Repeated Workflow with timer and failing activity with retries
+
+    """
     result = test_09()
     result = get_activity_result(result, 'activity4', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert [r for r in result.keys() if 'activity1' in r]
     assert [r for r in result.keys() if 'activity2' in r]
 
 
+@docprint
 def run_10():
-    # Activity fails several times due to heartbeat timeout, succeeds after retry
-    # Might print warnings
+    """Test 10
+    Activity fails several times due to heartbeat timeout, succeeds after retry
+    Might print warnings
+    """
+
     result = test_10()
     result = get_activity_result(result, 'activity5', 'v1')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['sleep_time'] == 0
 
 
+@docprint
 def run_11():
-    # Decider times out, succeeds after next decision task
-    # Prints a warning due to Decider timeout
+    """Test 11
+    Decider times out, succeeds after next decision task
+    Prints a warning due to Decider timeout
+    """
+
     result = test_11()
     result = get_activity_result(result, 'activity1', 'v5')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result['workflow'] == {'foo': 'bar'}
     assert result['status'] == 'finished'
 
 
+@docprint
 def run_12():
-    # run_09 with 2 parallel deciders
+    """Test 12
+    run_09 with 2 parallel deciders
+    """
+
     result = test_12()
     result = get_activity_result(result, 'activity4', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert [r for r in result.keys() if 'activity1' in r]
     assert [r for r in result.keys() if 'activity2' in r]
 
 
+@docprint
 def run_13():
-    # Two parallel deciders, one of them times out
+    """Test 13
+    Two parallel deciders, one of them times out
+
+    """
     result = test_13()
     result = get_activity_result(result, 'activity4', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert [r for r in result.keys() if 'activity1' in r]
     assert [r for r in result.keys() if 'activity2' in r]
 
+
+@docprint
 def run_14():
-    # Simple test with child workflow
+    """Test 14
+    Simple test with child workflow
+
+    """
     result = test_14()
     result = get_activity_result(result, 'test_child_workflow', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert [r for r in result.keys() if 'activity2' in r]
 
+
+@docprint
 def run_15():
+    """Test 15
+
+    """
     result = test_15()
     result = get_activity_result(result, 'test_child_workflow', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result
 
+
+@docprint
 def run_16():
+    """Test 16
+    Failing Task in ChildWorkflow
+
+    """
     result = test_16()
+    result = get_activity_result(result, 'test_child_workflow', 'v2')
+    print('Result: ' + json.dumps(result) + '\n')
     assert result
+
 
 tests = [run_01, run_02, run_03, run_04, run_05, run_06, run_07, run_08, run_09, run_10, run_11,
          run_12, run_13, run_14, run_15, run_16]
