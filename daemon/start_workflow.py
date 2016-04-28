36467d9ab7f9ec88a3fa6a69c4a7723c3751d381
Philipp <ph.pahl@gmail.com>
2016-04-28 18:02:59 +0200

    removed daemon folder

--- daemon/start_workflow.py
+++
@@ -1,8 +0,0 @@
-import floto
-
-floto.api.Swf().start_workflow_execution(domain='floto_test', 
-        workflow_id='daemon_test', 
-        workflow_type_name='my_workflow_type', 
-        workflow_type_version='v1', 
-        task_list='simple_decider')
-
