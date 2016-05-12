c5124f5b2a47e21c93aca0bfba8b0c9102b75a6a
Philipp <ph.pahl@gmail.com>
2016-05-12 12:08:55 +0200

    cosmetics and unit tests

--- floto/daemon/unix_daemon.py
+++ floto/daemon/unix_daemon.py
@@ -10,17 +10,39 @@ from signal import SIGTERM
 class UnixDaemon:
     """A generic unix daemon. This class needs to be inherited and the run method overwritten to 
     create a specific deamon.
-    It forks the current process to get a deamon.
-    See 'Python Cookbook', Martelli et al. O'Reilly, 2nd edition, page: 388
-    and https://github.com/ImmobilienScout24/succubus/blob/master/src/main/
-    python/succubus/daemonize.py """
+    
+    Example (my_daemon.py)
+    Run with: python my_daemon.py start|stop|restart|status
+    ----------------------
+
+    class MyDaemon(UnixDaemon):
+        def __init__(self):
+        super().__init__()
+        # Overwrite description, appears in e.g. python my_daemon.py -h
+        self.daemon_description = 'My daemon'
+
+        # Add you own script arguments.
+        def add_parse_arguments(self, parsre):
+            super().add_parse_arguments(parser)
+
+            # Example:
+            # Make log config available in self.args.log_config
+            parser.add_argument('--log_config', help='Path to logging config file')
+
+        def run(self):
+            # Your code
+            pass
+
+    if __name__ = '__main__':
+        MyDaemon().action()
+    """
 
     def __init__(self):
-        self._logger = None
         self._args = None
         self._arg_parser = None
         self._pid_file = None
-        self.daemon_description = 'Base unix daemon'
+        self._pid = None
+        self.description = 'Base unix daemon'
         self.logger = logging.getLogger(__name__)
 
     @property
@@ -36,7 +58,7 @@ class UnixDaemon:
     @property
     def arg_parser(self):
         if not self._arg_parser:
-            self._arg_parser = argparse.ArgumentParser(description=self.daemon_description)
+            self.arg_parser = argparse.ArgumentParser(description=self.description)
             self.add_parse_arguments(self._arg_parser)
         return self._arg_parser
 
@@ -47,7 +69,7 @@ class UnixDaemon:
     @property
     def pid_file(self):
         if self.args.pid_file:
-            self._pid_file = self.args.pid_file
+            self.pid_file = self.args.pid_file
         else:
             self._pid_file = '/tmp/unix_daemon.pid'
         return self._pid_file
@@ -55,6 +77,20 @@ class UnixDaemon:
     @pid_file.setter
     def pid_file(self, pid_file):
         self._pid_file = pid_file
+
+    @property
+    def pid(self):
+        if not self._pid:
+            try:
+                self.pid = int(open(self.pid_file).read().strip())
+            except FileNotFoundError as e:
+                self.logger.error('Can not read pid from file {}'.format(self.pid_file))
+                raise e
+        return self._pid
+
+    @pid.setter
+    def pid(self, pid):
+        self._pid = pid
 
     def add_parse_arguments(self, parser):
         """Definition of the command line arguments of the base daemon.
@@ -65,44 +101,87 @@ class UnixDaemon:
         parser.add_argument('command', help='The daemon command: start|status|stop|restart')
         parser.add_argument('--pid_file', help='The pid_file of the daemon')
     
-    def fork(self):
+    def create_child_process(self):
         try:
             pid = os.fork()
-            # If in parent process exit
+            # In parent process id of child > 0 is returned: exit parent
             if pid > 0:
                 os._exit(0)
         except OSError as err:
             self.logger.error('Fork failed.')
             os._exit(1)
 
-    def daemonize(self, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    def daemonize(self):
         """ Creates a daemon."""
-        # TODO: stdout and stderr to log files
-        self.fork()
+        self.create_child_process()
 
-        # decouple from parent environment with new session id
+        # Decouple from parent environment with new session id
         os.setsid()
         os.umask(0)
         os.chdir("/")
 
-        # Second fork to make process 'non session-leader' 
-        self.fork()
+        # Second child process to make process 'non session-leader' 
+        self.create_child_process()
 
-        # # TODO
-        # redirect standard file descriptors
-        # sys.stdout.flush()
-        # sys.stderr.flush()
-        # si = open(stdin, 'r')
-        # so = open(stdout, 'w')
-        # se = open(stderr, 'a+')
-        # os.dup2(si.fileno(), sys.stdin.fileno())
-        # os.dup2(so.fileno(), sys.stdout.fileno())
-        # os.dup2(se.fileno(), sys.stderr.fileno())
+    def run(self):
+        """to be overwritten"""
+        raise NotImplementedError
 
-        self.write_pid_file()
+    def start(self):
+        """Start the daemon."""
+        if self._pid_file_exists():
+            if self._is_running():
+                msg = 'Daemon already running: {}'.format(self.pid)
+            else:
+                msg = 'pid_file exists: {}, but process is not running.'.format(self.pid_file)
+            self.logger.error(msg)
+            raise RuntimeError(msg)
+        else:
+            self._start_daemon()
 
-    def write_pid_file(self):
-        # write current daemon pid to file
+    def stop(self):
+        """Stop the daemon."""
+        if self._pid_file_exists():
+            self._kill_process()
+            os.remove(self.pid_file)
+        else:
+            msg = 'Trying to stop daemon with non existing pid_file: {}'.format(self.pid_file)
+            self.logger.warning(msg)
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def status(self):
+        """Status of current daemon process. """
+        if self._pid_file_exists() and self._is_running():
+            print('DAEMON RUNNING -- pid: {}'.format(self.pid)) 
+
+        if self._pid_file_exists() and not self._is_running():
+            print('DAEMON NOT RUNNiNG -- pid_file exists: {}'.format(self.pid_file))
+
+        if not self._pid_file_exists():
+            print('DAEMON NOT RUNNING -- No pid_file: {}'.format(self.pid_file)) 
+
+
+    def action(self):
+        try:
+            getattr(self, self.args.command)()
+        except AttributeError:
+            msg = 'Command must be start|status|stop|restart. Was {}'.format(self.args.command)
+            self.logger.error(msg)
+            raise(AttributeError(msg))
+
+    def _start_daemon(self):
+        self.daemonize()
+        self._write_pid_file()
+        self.run()
+
+        # Finished run(), remove pid_file 
+        os.remove(self.pid_file)
+
+    def _write_pid_file(self):
+        """Write current daemon pid to file."""
         try:
             pid = str(os.getpid())
             self.logger.debug('Writing pid to file: {}'.format(pid))
@@ -113,95 +192,27 @@ class UnixDaemon:
             msg = 'IOError writing pid to pid file ({0}): {1}'.format(err.errno, err.strerror)
             self.logger.error(msg)
 
-    def run(self):
-        """to be overwritten"""
-        raise NotImplementedError
+    def _pid_file_exists(self):
+        return True if os.path.isfile(self.pid_file) else False
 
-    def stop(self):
-        """Stop the daemon."""
-        pid = self.get_pid_from_file()
-        self._delete_pid_file()
-        if pid:
-            self._kill_process(pid)
-
-    def get_pid_from_file(self):
-        """Returns pid of current daemon."""
-        pid = None
-        if os.path.isfile(self.pid_file):
-            try:
-                pid = int(open(self.pid_file).read().strip())
-            except:
-                self.logger.error('Problems reading in pid from pid file')
-        else:
-            self.logger.error('pid_file does not exist: {}'.format(self.pid_file))
-        return pid
-
-    def start(self):
-        """Start the daemon."""
-        if os.path.isfile(self.pid_file):
-            pid = self.get_pid_from_file()
-            if self._pid_exists(pid):
-                msg = 'Daemon already running: {}'.format(pid)
-                self.logger.error(msg)
-                raise Exception(msg)
-            else:
-                msg = 'pid_file exists: {}, but process is not running.'.format(self.pid_file)
-                self.logger.error(msg)
-                raise Exception(msg)
-        else:
-            self.daemonize()
-            self.run()
-
-            # Finished run(), remove pid_file 
-            self._delete_pid_file() 
-
-    def status(self):
-        """Status of current daemon process. """
-        pid = self.get_pid_from_file()
-        if pid:
-            if self._pid_exists(pid):
-                sys.stdout.write('daemon running with pid: ' + str(pid) + '\n')
-                sys.stdout.write('path to pid file: ' + self.pid_file + '\n')
-            else:
-                sys.stdout.write('Something wrong, process ' + str(pid) + ' not alive anymore. \n')
-        else:
-            sys.stdout.write('No daemon running \n')
-
-    def restart(self):
-        self.stop()
-        self.start()
-
-    def action(self):
-        try:
-            getattr(self, self.args.command)()
-        except AttributeError:
-            msg = 'Command must be start|status|stop|restart. Was {}'.format(self.args.command)
-            self.logger.error(msg)
-
-    def _pid_exists(self, pid):
-        """Check for the existence of a unix pid."""
+    def _is_running(self):
+        """Check if process with self.pid is running."""
         try:
             # Process is not killed, os.kill(pid, 0) does nothing but raise if process does not
             # exist.
-            os.kill(pid, 0)
+            os.kill(self.pid, 0)
         except ProcessLookupError:
             return False
         else:
             return True
 
-    def _kill_process(self, pid):
+    def _kill_process(self):
         try:
             for _ in range(100):
-                os.kill(pid, SIGTERM)
+                os.kill(self.pid, SIGTERM)
                 time.sleep(0.1)
         except ProcessLookupError:
             return
         except OSError as err:
             self.logger.error(msg)
-
-    def _delete_pid_file(self):
-        try:
-            os.remove(self.pid_file)
-        except FileNotFoundError:
-            self.logger.error('pid_file does not exist: {}'.format(self.pid_file))
 
--- tests/unit/daemon/test_unix_daemon.py
+++ tests/unit/daemon/test_unix_daemon.py
@@ -1,30 +1,48 @@
-import logging
 import pytest
+from unittest.mock import Mock
+
 import floto.daemon
 from collections import namedtuple
 import random
-import time
 import os.path
+import argparse
+import tempfile
 
 class TestDaemon(floto.daemon.UnixDaemon):
-    def __init__(self, command, pid_file=None):
+    def __init__(self, command='start', pid_file='file.pid'):
         super().__init__()
-        args = {'command':command,
-                'pid_file':pid_file}
-        self.args = namedtuple('args', args.keys())(**args)
-        self.logger = logging.getLogger('TestLogger')
-        self.run_ongoing_task = False
-
-    def run(self):
-        self.logger.debug('run()')
-        while self.run_ongoing_task:
-            time.sleep(1)
+        self._args = self.arg_parser.parse_args([command, '--pid_file', pid_file])
 
     # Do not fork for testing
     def daemonize(self):
-        self.write_pid_file()
+        pass
+
+    def run(self):
+        pass
 
 class TestUnixDaemon:
+    def test_args(self):
+        d = TestDaemon()
+        assert d.arg_parser.description == d.description
+        assert set(vars(d.args).keys()) == set(['command', 'pid_file'])
+
+    def test_default_pid_file_name(self):
+        d = TestDaemon() 
+        d.args.pid_file = None
+        assert d.pid_file == '/tmp/unix_daemon.pid'
+
+    def test_get_pid_from_pid_file(self):
+        with tempfile.NamedTemporaryFile(mode='w') as pid_file:
+            pid_file.write('1234')
+            pid_file.seek(0)
+            d = TestDaemon('start', pid_file=pid_file.name)
+            assert d.pid == 1234
+
+    def test_pid_raises(self):
+        with pytest.raises(FileNotFoundError) as e:
+            d = TestDaemon('start', pid_file='pid_file_does_not_exist.pid')
+            d.pid
+
     def test_start_is_called(self, mocker):
         mocker.patch('floto.daemon.UnixDaemon.start')
         d = floto.daemon.UnixDaemon()
@@ -39,3 +57,92 @@ class TestUnixDaemon:
         test_daemon.action()
         assert not os.path.isfile(test_daemon.pid_file)
 
+    def test_start_daemonizes(self):
+        d = TestDaemon(command='start')
+        d._pid_file_exists = Mock(return_value=False)
+        d._start_daemon = Mock()
+        d.action()
+        d._start_daemon.assert_called_once_with()
+
+    def test_start_pid_file_exists_process_running_raises(self):
+        d = TestDaemon(command='start')
+        d._pid_file_exists = Mock(return_value=True)
+        d._is_running = Mock(return_value=True)
+        d._pid = '1234'
+        with pytest.raises(RuntimeError):
+            d.action()
+
+    def test_start_pid_file_exists_process_not_running_raises(self):
+        d = TestDaemon(command='start')
+        d._pid_file_exists = Mock(return_value=True)
+        d._is_running = Mock(return_value=False)
+        d._pid = '1234'
+        with pytest.raises(RuntimeError):
+            d.action()
+
+    def test_stop_with_existing_pid_file(self):
+        pid_file = '/tmp/{}.pid'.format(str(random.randint(0, 100000)))
+        open(pid_file, 'w').write('123456789')
+
+        d = TestDaemon(command='stop', pid_file=pid_file)
+        d._kill_process = Mock()
+        d.action()
+        d._kill_process.assert_called_once_with()
+        assert not d._pid_file_exists()
+
+    def test_stop_without_pid_file_does_not_raise(self):
+        d = TestDaemon(command='stop')
+        d.stop()
+
+    def test_restart(self):
+        d = TestDaemon()
+        d.stop = Mock()
+        d.start = Mock()
+        d.restart()
+        d.stop.assert_called_once_with()
+        d.start.assert_called_once_with()
+
+    def test_status_no_pid_file(self, capsys):
+        d = TestDaemon()
+        d._pid_file_exists = Mock(return_value=False)
+        d.status()
+        out, err = capsys.readouterr()
+        assert out.strip() == 'DAEMON NOT RUNNING -- No pid_file: file.pid'
+
+    def test_status_pid_file_and_not_running(self, capsys):
+        d = TestDaemon()
+        d._pid_file_exists = Mock(return_value=True)
+        d._is_running = Mock(return_value=False)
+        d.status()
+        out, err = capsys.readouterr()
+        assert out.strip() == 'DAEMON NOT RUNNiNG -- pid_file exists: file.pid'
+
+    def test_status_pid_file_and_running(self, capsys):
+        d = TestDaemon()
+        d._pid_file_exists = Mock(return_value=True)
+        d._is_running = Mock(return_value=True)
+        d.pid = '1234'
+        d.status()
+        out, err = capsys.readouterr()
+        assert out.strip() == 'DAEMON RUNNING -- pid: 1234'
+
+    def test_action_calls(self, mocker):
+        d = TestDaemon(command='start')
+        d.start = Mock()
+        d.action()
+        d.start.assert_called_once_with()
+
+    def test_action_raises_with_unknow_command(self, mocker):
+        d = TestDaemon(command='do_something')
+        with pytest.raises(AttributeError):
+            d.action()
+
+    def test_write_pid_file(self, mocker):
+        mocker.patch('os.getpid', return_value=1234)
+        with tempfile.NamedTemporaryFile(mode='w') as pid_file:
+            d = TestDaemon(pid_file=pid_file.name)
+            d._write_pid_file()
+            assert d._pid_file_exists()
+            pid = open(pid_file.name).read().strip()
+            assert pid == '1234'
+        
