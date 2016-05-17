266e42fb4b3903d065703b4d6f45ce37a42ca469
philipppahl <ph.pahl@gmail.com>
2016-05-17 14:26:55 +0200

    UnixDaemon and examples for decider and worker

--- examples/daemon/activity_worker_daemon.py
+++ examples/daemon/activity_worker_daemon.py
@@ -3,22 +3,32 @@ import importlib.util
 
 import floto
 from floto.daemon import UnixDaemon
+import os
+import yaml
+
+@floto.activity(domain='floto_test', name='simple_activity', version='v1')
+def simple_activity():
+    print('\nSimpleWorker: I\'m working!')
+    for i in range(3):
+        print('.')
+        time.sleep(0.8)
+
+    # Terminate the worker after first execution:
+    print('I\'m done.')
 
 class ActivityWorkerDaemon(UnixDaemon):
+    def __init__(self):
+        super().__init__()
+        self.daemon_description = 'Daemon to run floto worker to copy S3 folders.'
+        self.configure_logger()
+
     def add_parse_arguments(self, parser):
         super().add_parse_arguments(parser)
         parser.add_argument('domain', help='SWF domain of the worker')
         parser.add_argument('task_list', help='Worker task list')
-        parser.add_argument('activities', help='Path to activity function definition')
         parser.add_argument('--identity', help='Identity of the worker')
         parser.add_argument('--heartbeat', help='Task heatbeat in seconds')
         parser.add_argument('--log_config', help='Path to the logging config file')
-
-    def register_activities(self):
-        # TODO read from s3
-        spec = importlib.util.spec_from_file_location('activities', self.args.activities)
-        activities = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(activities)
 
     def get_worker(self):
         args = {'domain':self.args.domain,
@@ -34,15 +44,17 @@ class ActivityWorkerDaemon(UnixDaemon):
 
     def run(self):
         self.logger.debug('starting worker')
-        self.register_activities()
         worker = self.get_worker()
         worker.run()
 
-    def configure_logger(self, path):
+    def configure_logger(self):
+        path = self.args.log_config if self.args.log_config else ''
         if path.endswith('.yml'):
             if os.path.exists(path):
                 with open(path, 'rt') as f:
                     config = yaml.safe_load(f.read())
+            else:
+                raise ValueError('Path does not exist: {}'.format(path))
             logging.config.dictConfig(config)
 
 def main():
